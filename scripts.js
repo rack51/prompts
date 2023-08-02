22f335cee5e0ae2f5e912c486d9d69be67932504
Srujan <acsrujan@gmail.com>
2023-08-02 11:09:09 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,49 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
   const promptText = document.getElementById('prompt-text');
   const nextPromptButton = document.getElementById('next-prompt');
+  
+  let displayedPrompts = []; // To track displayed prompts
 
-  // Function to fetch and display the next prompt
-  async function fetchAndDisplayNextPrompt() {
+  // Function to fetch the next prompt that hasn't been displayed
+  async function fetchNextPrompt() {
     try {
-      // Fetch the next prompt from Supabase
-      const response = await fetch('/api/get-next-prompt');
+      const response = await fetch('https://wssdtetyjafznlphnrmb.supabase.co/rest/v1/prompts', {
+        headers: {
+          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzc2R0ZXR5amFmem5scGhucm1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3MTI2MTksImV4cCI6MjAwNjI4ODYxOX0.xkFd8YYPahe90YDjBsdks1Co9GjcaudFz2BTWsfDTV0',
+          'Prefer': 'return=representation',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch prompt.');
+      }
+
       const data = await response.json();
 
-      if (data && data.prompt_text) {
-        promptText.textContent = data.prompt_text;
-        // Mark the prompt as displayed (update 'displayed' to true)
-        await markPromptAsDisplayed(data.id);
+      return data;
+    } catch (error) {
+      console.error('Error fetching prompt:', error);
+      return null;
+    }
+  }
+
+  // Display the next prompt
+  async function displayNextPrompt() {
+    try {
+      const prompt = await fetchNextPrompt();
+
+      if (prompt) {
+        promptText.textContent = prompt[0].prompt_text;
+        displayedPrompts.push(prompt[0].id);
       } else {
         promptText.textContent = 'No more prompts available.';
         nextPromptButton.disabled = true;
       }
     } catch (error) {
-      console.error('Error fetching prompt:', error);
+      console.error('Error displaying prompt:', error);
       promptText.textContent = 'Error fetching prompt.';
     }
   }
 
-  // Function to mark the prompt as displayed (update 'displayed' to true)
-  async function markPromptAsDisplayed(promptId) {
-    try {
-      const response = await fetch('/api/mark-prompt-as-displayed', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ promptId }),
-      });
+  // Handle "Next Prompt" button click
+  nextPromptButton.addEventListener('click', displayNextPrompt);
 
-      if (!response.ok) {
-        throw new Error('Failed to mark prompt as displayed.');
-      }
-    } catch (error) {
-      console.error('Error marking prompt as displayed:', error);
-    }
-  }
-
-  nextPromptButton.addEventListener('click', fetchAndDisplayNextPrompt);
-
-  // Fetch and display the first prompt when the page loads
-  fetchAndDisplayNextPrompt();
+  // Display the first prompt when the page loads
+  displayNextPrompt();
 });
